0f2db9e2beb5477ad15de50f8796ba6f66aad86a
marghe27 <marghe27@live.it>
2024-07-01 23:38:05 +0200

    Feat: add fetch service

---
+++ src/app/mock/messages.ts
@@ -0,0 +1,9 @@
+import { Message } from "../models/message";
+
+export const MOCK_MESSAGES: Message[] = [
+    { id: 1, type: 'text', date: '07/01/2024', status: 'Normal', amount: 500, user_id: 1},
+    { id: 2, type: 'audio', date: '07/01/2024', status: 'High', amount: 1500, user_id: 2}
+]
+    
+    
+    
---
+++ src/app/models/message.ts
@@ -0,0 +1,8 @@
+export interface Message {
+    id: number;
+    type: string;
+    date: string;
+    status: string;
+    amount: number;
+    user_id: number;
+}
