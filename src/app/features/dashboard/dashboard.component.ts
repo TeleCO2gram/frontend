f4b319492288d536d25b7839844b087b9cac2d64
Margherita <39591532+marghe27@users.noreply.github.com>
2024-07-21 18:49:48 +0000

    feat: create dashboard component

---
+++ src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from "../../core/components/nav-bar/nav-bar.component";
+import { ChartPieComponent } from '../../chart-pie/chart-pie.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    NavBarComponent,
+    ChartPieComponent,
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
