960bd95d4d706eb0fb056b1041500439c348932e
Margherita <39591532+marghe27@users.noreply.github.com>
2024-07-21 18:50:38 +0000

    feat: create nav bar component

---
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+
+  constructor(private router: Router){}
+
+}
